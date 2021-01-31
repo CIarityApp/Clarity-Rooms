d06df445a5894a2fe929493deba8d0bdef27fa41
Andy Wang <andynmnwang@gmail.com>
2021-01-30 19:04:41 -0600

    redo codes in node

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const http = require('http')
+
+const app = express()
+const router = express.Router()
+
+router.get('/rooms/:roomId', (req, res) => {
+  if(req.params.roomId == "ABCD") {
+    res.status(200).json({ receiver: "THIS SHOULD BE A FIREBASE REALTIME DB URL" })
+  } else {
+    res.status(401).json({ error: "Invalid code" })
+  }
+})
+app.use('/', router)
+
+const server = http.createServer(app)
+server.listen(8080, () => {
+  console.log('Listening on http://localhost:' + 8080)
+})
