d5868442051b6077aa98e1c210ba4bb5dc5b9f21
Andy Wang <andynmnwang@gmail.com>
2021-01-31 05:50:33 -0600

    add support for multipart upload (testing)

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 require('dotenv').config()
 const express = require('express')
+const multer = require('multer')
 const http = require('http')
 
 const app = express()
 const router = express.Router()
+const upload = multer({ dest: 'assets/' })
+
+app.post('/upload', upload.single('file'), (req, res, next) => {
+  console.log(req.file.filename)
+  console.log(req.file.mimetype)
+  console.log(req.file.size)
+  res.status(200).json({ res: 'Success' })
+})
 
 router.get('/rooms/:roomId', (req, res) => {
   if(req.params.roomId == "ABCD") {
@@ -15,6 +24,7 @@ router.get('/rooms/:roomId', (req, res) => {
 app.use('/', router)
 
 const server = http.createServer(app)
-server.listen(process.env.PORT, () => {
-  console.log('Listening on http://localhost:' + process.env.PORT)
+const port = process.env.PORT || 8080
+server.listen(port, () => {
+  console.log('Listening on http://localhost:' + port)
 })
